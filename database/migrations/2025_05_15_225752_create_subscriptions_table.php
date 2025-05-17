150a4da1977f683f55d47429a9ecbba3006ff985
Wesley <cardoso.tads@gmail.com>
2025-05-16 22:59:42 -0400

    wip

---
+++ database/migrations/2025_05_15_225752_create_subscriptions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUlid('project_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('customer_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('product_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->timestamps();
+            $table->timestamp(column: 'trial_ends_at')->nullable();
+            $table->timestamp(column: 'grace_ends_at')->nullable();
+            $table->timestamp('starts_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
